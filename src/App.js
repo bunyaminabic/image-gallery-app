8ed21cf37d3d41bf085a457a4f0ce21a61cd9062
Bünyamin Abiç <bunyamin.abic@outlook.com>
2024-04-28 18:56:57 +0300

    image gallery application was made

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Images from "./Images";
+import "./styles.css";
+
+function App() {
+  const [selectedImg, setSelectedImg] = useState(Images[0]);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={selectedImg} alt="selected" className="selected" />
+      </div>
+
+      <div className="imgContainer">
+        {Images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt="tantuni"
+            onClick={() => setSelectedImg(img)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
